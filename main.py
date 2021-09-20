57a0571bd4488d82a75cd944c970f6d3b6437c27
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-09-20 08:33:29 +0200

    Add GitHub actions (#2)

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
-import sys
-sys.path.insert(0, './code')
-import data
+
+from code import data
 
 app = FastAPI()
 # Load the data in dataframe format
 df = data.getDataframe()
 
+
 @app.get('/')
 def root():
-    print(list(df.keys()))
     return {'message': 'Hello world!'}
+
 
 @app.get('/tests')
 def list_tests():
-    print("Number of test unique: ", len(list(df['manufacturer'].unique())))
+    print("Number of manufacturers of covid tests unique: ", len(list(df['manufacturer'].unique())))
     return list(df['manufacturer'].unique())
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', reload=True)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 
 from fastapi.testclient import TestClient
@@ -6,6 +7,13 @@ from main import app
 
 
 class TestCovidEndpoints(TestCase):
+
+    def test_root_endpoint_works(self):
+        client = TestClient(app)
+
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
 
     def test_list_endpoint_works(self):
         client = TestClient(app)
@@ -20,5 +28,9 @@ class TestCovidEndpoints(TestCase):
         response = client.get('/tests1')
 
         self.assertEqual(response.status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
