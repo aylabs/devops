2a1bfcfa3545ed8bd5e7a8bdc998457e01fe5242
Javi <jjbd98@hotmail.com>
2021-10-01 11:11:29 +0200

    list_test (API) return a list of manufacturers

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World...")
+import json
+import uvicorn
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {'message': 'Hello World'}
+
+@app.get("/tests")
+def list_tests():
+    data = json.load(open('./data/device-covid19serology-0001-of-0001.json'))
+    df = pd.DataFrame(data["results"])
+
+    return df.manufacturer
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', reload=True)
