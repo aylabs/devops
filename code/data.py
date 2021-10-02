c39d189e6b8a82a198c4303d9d006262baeb71d7
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2021-10-02 12:51:32 +0200

    API implementation: /tests (I return the list of unique tests)

--- code/data.py
+++ code/data.py
@@ -1,15 +1,21 @@
 import json
 import pandas as pd
 
-print("Converting data from json to csv...")
+# print("Converting data from json to csv...")
 
 """ JSON TO DATAFRAME """
 
-data = json.load(open('../data/device-covid19serology-0001-of-0001.json'))
+# data = json.load(open('../data/device-covid19serology-0001-of-0001.json'))
 
-df = pd.DataFrame(data["results"])
+# df = pd.DataFrame(data["results"])
 """print(df)"""
 
 """ DATAFRAME TO CSV """
 
-df.to_csv('../data/device-covid19serology-0001-of-0001.csv', index = None)
+# df.to_csv('../data/device-covid19serology-0001-of-0001.csv', index = None)
+
+# I comment the content of the file and adapt it to a function that returns the dataframe from the data.
+
+def getDataframe():
+    data = json.load(open('./data/device-covid19serology-0001-of-0001.json'))
+    return pd.DataFrame(data["results"])
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+import sys
+sys.path.insert(0, './code')
+import data
 
 app = FastAPI()
+# Load the data in dataframe format
+df = data.getDataframe()
 
 @app.get('/')
 def root():
-    return {'message': 'Hello World'}
+    print(list(df.keys()))
+    return {'message': 'Hello world!'}
 
 @app.get('/tests')
 def list_tests():
-    return ['test1', 'test2', 'test3']
+    print("Number of test unique: ", len(list(df['manufacturer'].unique())))
+    return list(df['manufacturer'].unique())
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', reload=True)
