aa30ce3f654baf97e4ee36d16c65328c4068698f
rssoroka <r.soroka.2020@alumnos.urjc.es>
2021-10-03 12:27:20 +0200

    Modify to extract data from json.

--- code/data.py
+++ code/data.py
@@ -5,11 +5,16 @@ print("Converting data from json to csv...")
 
 """ JSON TO DATAFRAME """
 
-data = json.load(open('../data/device-covid19serology-0001-of-0001.json'))
+#data = json.load(open('../data/device-covid19serology-0001-of-0001.json'))
 
-df = pd.DataFrame(data["results"])
+#df = pd.DataFrame(data["results"])
 """print(df)"""
 
 """ DATAFRAME TO CSV """
 
-df.to_csv('../data/device-covid19serology-0001-of-0001.csv', index = None)
+#df.to_csv('../data/device-covid19serology-0001-of-0001.csv', index = None)
+
+""" GET DATAFRAME"""
+def getDataframe():
+    data_covid = json.load(open('./data/device-covid19serology-0001-of-0001.json'))
+    return pd.DataFrame(data_covid["results"])
