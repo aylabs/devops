b385078294ecde233b8c7b485fa72fb946b2910c
rssoroka <r.soroka.2020@alumnos.urjc.es>
2021-10-03 12:27:55 +0200

    Modify to extract data from json, and modify README file with new information.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello :)")
+import uvicorn
+from fastapi import FastAPI
+import sys
+
+sys.path.insert(0, './code')
+import data
+
+data_frame = data.getDataframe()
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {'message': 'Its test!'}
+
+@app.get('/tests')
+def list_tests():
+    print("Data contain", len(list(data_frame)), "categories")
+    return data_frame
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', reload=True)
