bf4058a213a6e6c61309692e0f51f74938c2af0c
Javi <jjbd98@hotmail.com>
2021-10-04 11:20:55 +0200

    list_test (API) return a list of manufacturers and created function getDataframe

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import json
 import uvicorn
-import pandas as pd
 from fastapi import FastAPI
+import sys
+sys.path.insert(0, './code')
+import data
 
 app = FastAPI()
 
+df = data.getDataframe()
+
 @app.get("/")
 def root():
-    return {'message': 'Hello World'}
+    return ["root"]
 
 @app.get("/tests")
 def list_tests():
-    data = json.load(open('./data/device-covid19serology-0001-of-0001.json'))
-    df = pd.DataFrame(data["results"])
-
     return df.manufacturer
 
 if __name__ == '__main__':
