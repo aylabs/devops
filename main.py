8e4fa4eda658288dfcab32a4a79c9b80e7614086
oscar <47352810+oscarescuderoarnanz@users.noreply.github.com>
2021-10-11 12:59:29 +0200

    Implementation of a minimal web portal consuming our REST API #16 (continued) (#50)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from code import data
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # Load the data in dataframe format
@@ -20,8 +22,8 @@ def root(request:Request):
 def list_tests(request:Request):
     print("Number of manufacturers of covid tests unique: ", len(list(df['manufacturer'].unique())))
     manufacturers = list(df['manufacturer'].unique())
-    print(manufacturers)
     return templates.TemplateResponse("tests_manufacturers.html", {"request": request, "list": manufacturers})
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', reload=True)
