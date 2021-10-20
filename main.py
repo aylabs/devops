22d00cd2631a2f971063d0abef42c64e0cd08d16
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-10-20 07:24:35 +0200

    Add docker-compose for swarm mode and the link to the project

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ df = data.getDataframe()
 
 
 @app.get('/')
-def root(request:Request):
-    return templates.TemplateResponse("root.html", {"request":request})
+def root(request: Request):
+    return templates.TemplateResponse("root.html", {"request": request})
 
 
 @app.get('/tests')
-def list_tests(request:Request):
+def list_tests(request: Request):
     print("Number of manufacturers of covid tests unique: ", len(list(df['manufacturer'].unique())))
     manufacturers = list(df['manufacturer'].unique())
     return templates.TemplateResponse("tests_manufacturers.html", {"request": request, "list": manufacturers})
