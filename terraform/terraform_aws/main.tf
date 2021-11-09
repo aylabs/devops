b97fec4e250e00b03f2dd7df0b97f811aa030415
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-11-09 06:14:33 +0100

    Install docker in EC2 instances nodes

--- terraform/terraform_aws/main.tf
+++ terraform/terraform_aws/main.tf
@@ -18,13 +18,40 @@ resource "aws_security_group" "ssh" {
   }
 }
 
+resource "aws_security_group" "http_out" {
+  name = "allow_http_out"
+  description = "Allow HTTP/S connections"
+
+  egress {
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "swarm_node" {
   count=3
   # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
   ami = "ami-08edbb0e85d6a0a07"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ssh.id]
+  vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.http_out.id]
   key_name = var.key_name
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Check" > test.log
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    adduser ubuntu docker
+  EOF
 
   tags = {
     Name = "swarm"
